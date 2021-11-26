2feebf3a9d7c0d53deaefa8775d060b5b08a2657
find11570 <find11570@kumoh.ac.kr>
2021-11-26 23:34:44 +0900

    파일 동영상 등수삭조 임시

--- src/components/Dashboard/FileViewer.js
+++ src/components/Dashboard/FileViewer.js
@@ -1,31 +1,166 @@
-import {
-	Card,
-	CardContent,
-} from '@material-ui/core';
+import { Card, CardContent, Box, Button } from '@material-ui/core';
 import { useState } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import Api from '../../Api/Api';
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+const server_path = 'http://202.31.202.28:443/file/';
 
 const FileViewer = () => {
 	const [chartData] = useState({
 		content: '파일뷰어 들어갈자리.'
 	});
+	const [numPages, setNumPages] = useState(null);
+	const [pageNumber, setPageNumber] = useState(1);
+
+	function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+    setPageNumber(1);
+	}
+
+	function changePage(offset) {
+		setPageNumber((prevPageNumber) => prevPageNumber + offset);
+	}
+
+	function previousPage() {
+		changePage(-1);
+	}
+
+	function nextPage() {
+		changePage(1);
+	}
+
+	const [file, setFile] = useState();
+	const [fileUrl, setFileUrl] = useState(null);
+
+	const processFile = async (event) => {
+		const documentFile = event.target.files[0];
+		const documentUrl = URL.createObjectURL(documentFile);
+		setFile(documentFile);
+		setFileUrl(documentUrl);
+		const formData = new FormData();
+		formData.append('attachments', documentFile);
+		let response = await Api.getReadFile(formData);
+		if (response.sucess) {
+			let document_path = response.files[0].file_path.replace('file\\', '')
+			let document = server_path + document_path;
+			setFileUrl(document);
+		} else {
+			console.log('파일 업로드 실패');
+		}
+	};
+
+	const deleteFile = (event) => {
+		setFileUrl('');
+		setFile(null);
+	};
 
 	return (
-		<Card
-			sx={{
-				borderBottomRightRadius: 10,
-				borderBottomLeftRadius: 10,
-				borderTopRightRadius: 10,
-				borderTopLeftRadius: 10,
-				boxShadow: 5
-			}}
-		>
-			<CardContent>
-				<h3>
-					{chartData.content}
+    <Card
+      sx={{
+        borderBottomRightRadius: 10,
+        borderBottomLeftRadius: 10,
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        boxShadow: 5
+      }}
+    >
+      <CardContent>
+        <div className="file__box">
+			<Box
+				sx={{
+					minHeight: '100%',
+					py: 1.5
+				}}
+			/>
+			<Button
+				variant="contained"
+				size="small"
+				color="info"
+				sx={{
+					marginTop: 2,
+					width: 180
+				}}
+			>
+				<label
+					htmlFor="file"
+					style={{
+						width: 100
+					}}
+				>
+					<h3
+						style={{
+							color: '#ffffff'
+						}}
+					>
+						파일 업로드
+					</h3>
+				</label>
+				<input
+					type="file"
+					id="file"
+					style={{
+						color: '#ffffff',
+						display: 'none'
+					}}
+					onChange={processFile}
+				></input>
+			</Button>
+			<Box
+				sx={{
+					minHeight: '100%',
+					py: 0.5
+				}}
+			/>
+			<Button
+				variant="contained"
+				size="small"
+				color="success"
+				sx={{
+					marginTop: 2,
+					width: 180
+				}}
+				onClick={deleteFile}
+			>
+				<h3
+					style={{
+						color: '#ffffff'
+					}}
+				>
+					파일 삭제
 				</h3>
-			</CardContent>
-		</Card>
-	);
+			</Button>
+		</div>
+
+        <Document
+          file={fileUrl}
+          onLoadError={console.error}
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <div>
+          <p>
+            Page {pageNumber || (numPages ? 1 : '--')} of {numPages || '--'}
+          </p>
+          <button
+            type="button"
+            disabled={pageNumber <= 1}
+            onClick={previousPage}
+          >
+            Previous
+          </button>
+          <button
+            type="button"
+            disabled={pageNumber >= numPages}
+            onClick={nextPage}
+          >
+            Next
+          </button>
+        </div>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default FileViewer;
--- src/components/Dashboard/Video.js
+++ src/components/Dashboard/Video.js
@@ -1,23 +1,47 @@
 
-import {
-	Card,
-	CardContent,
-} from '@material-ui/core';
+import { Card, CardContent, Box, Button } from '@material-ui/core';
 import { useState } from 'react';
 import ReactPlayer from 'react-player';
-import video from 'src/components/Dashboard/video.mp4';
+import Api from '../../Api/Api';
+
+const server_path = 'http://202.31.202.28:443/file/';
 
 const Video = () => {
 	const [chartData] = useState({
 		videoPath: 'file:///C:\Users\Administrator\Documents\GitHub\se-manager\src\components\Dashboard\video.mp4'
 	});
 
+	const [videofile, setVideofile] = useState();
+	const [fileUrl, setFileUrl] = useState(null);
+
+	const processVideo = async (event) => {
+		const videoFile = event.target.files[0];
+		const videoUrl = URL.createObjectURL(videoFile);
+		setVideofile(videoFile);
+		setFileUrl(videoUrl);
+		const formData = new FormData();
+		formData.append('attachments', videoFile);
+		let response = await Api.getReadFile(formData);
+		if (response.sucess) {
+			let video_path = response.files[0].file_path.replace('file\\', '')
+			let video = server_path + video_path;
+			setFileUrl(video);
+		} else {
+			console.log('비디오 업로드 실패');
+		}
+	};
+
+	const deleteVideo = (event) => {
+		setFileUrl('');
+		setVideofile(null);
+	};
+
 	function Screen() {
 		return (
 			<div className='player-wrapper'>
 			  <ReactPlayer
 				className='react-player fixed-bottom'
-				url= {video}
+				url= {fileUrl}
 				width='100%'
 				height='100%'
 				controls = {true}
@@ -37,6 +61,73 @@ const Video = () => {
 			}}
 		>
 			<CardContent>
+				<div className="video__box">
+					<Box
+						sx={{
+							minHeight: '100%',
+							py: 1.5
+						}}
+					/>
+					<Button
+						variant="contained"
+						size="small"
+						color="info"
+						sx={{
+							marginTop: 2,
+							width: 180
+						}}
+					>
+						<label
+							htmlFor="file"
+							style={{
+								width: 100
+							}}
+						>
+							<h3
+								style={{
+									color: '#ffffff'
+								}}
+							>
+								비디오 업로드
+							</h3>
+						</label>
+						<input
+							type="file"
+							id="file"
+							accept="video/*"
+							style={{
+								color: '#ffffff',
+								display: 'none'
+							}}
+							onChange={processVideo}
+						></input>
+					</Button>
+					<Box
+						sx={{
+							minHeight: '100%',
+							py: 0.5
+						}}
+					/>
+					<Button
+						variant="contained"
+						size="small"
+						color="success"
+						sx={{
+							marginTop: 2,
+							width: 180
+						}}
+						onClick={deleteVideo}
+					>
+						<h3
+							style={{
+								color: '#ffffff'
+							}}
+						>
+							비디오 삭제
+						</h3>
+					</Button>
+				</div>
+
 				{Screen()}
 			</CardContent>
 		</Card>
